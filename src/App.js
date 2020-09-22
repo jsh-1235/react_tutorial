8665b67e504b16dd2b19fae088afa711e2f470ac
jsh <jsh-1235@daum.net>
2020-09-22 15:39:46 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,205 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 
+import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import Home from "./pages/Home";
+import SpeedDials from "./pages/SpeedDials";
+import Ratings from "./pages/Ratings";
+import NotFound from "./pages/NotFount";
+
+//==================================================================
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { green, orange } from "@material-ui/core/colors";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+import clsx from "clsx";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import FolderIcon from "@material-ui/icons/Folder";
+import RestoreIcon from "@material-ui/icons/Restore";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: green[500],
+    },
+    secondary: {
+      main: orange[500],
+    },
+  },
+});
+
+const useStyles = makeStyles((theme) => ({
+  header: {
+    flexGrow: 1,
+  },
+  main: {
+    flexGrow: 1,  
+    padding: "10px",
+  },
+  footer: {
+    position: "fixed",
+    left: "0",
+    bottom: "0",
+    width: "100%",
+    padding: "0px",
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  linkButton: {
+    marginRight: theme.spacing(2),
+  },
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: "auto",
+  },
+}));
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hi React!!!</h1>
-        <p>My name is jsh.</p>
-        <p>담배 피고 올께 ㅋㅋㅋ</p>
-        <p>오줌 말려...</p>
-      </header>
+  const classes = useStyles();
+
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
+      return;
+    }
+    setState({
+      ...state,
+      [anchor]: open,
+    });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === "top" || anchor === "bottom",
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
     </div>
+  );
+
+  const [value, setValue] = React.useState("recents");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    // console.info(newValue);
+    alert(newValue);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <header>
+          <AppBar position="static" className={classes.header}>
+            <Toolbar>
+              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                Menu
+              </Typography>
+              <Link color="inherit" to="/">
+                <Button variant="outlined" className={classes.linkButton}>
+                  Home
+                </Button>
+              </Link>
+              <Link color="inherit" to="/speeddials">
+                <Button variant="outlined" className={classes.linkButton}>
+                  Speed Dial
+                </Button>
+              </Link>
+              <Link color="inherit" to="/ratings">
+                <Button variant="outlined" className={classes.linkButton}>
+                  Rating
+                </Button>
+              </Link>
+            </Toolbar>
+          </AppBar>
+        </header>
+
+        <div>
+          {["left", "right", "top", "bottom"].map((anchor) => (
+            <React.Fragment key={anchor}>
+              <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+              <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+                {list(anchor)}
+              </Drawer>
+            </React.Fragment>
+          ))}
+        </div>
+
+        <main className={classes.main}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/speeddials" component={SpeedDials} />
+            <Route path="/ratings" component={Ratings} />
+            <Route component={NotFound} />
+          </Switch>
+        </main>
+        <footer className={classes.footer}>
+          <BottomNavigation value={value} onChange={handleChange} className={classes.root}>
+            <BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} />
+            <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
+            <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} />
+            <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+          </BottomNavigation>
+        </footer>
+      </Router>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
+// 다른 주소에 따라 다른 뷰를 보여주는것을 라우팅 이라고 한다.
